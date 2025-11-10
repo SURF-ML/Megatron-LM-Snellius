801e8ea1c6478495b1946831990826678b8d6afb
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2025-11-10 10:29:25 +0100

    fix versioning

--- data/download_data.py
+++ data/download_data.py
@@ -1,5 +1,6 @@
 import os
 from datasets import load_dataset
+from datasets import load_dataset_builder
 
 project_space = os.environ.get("PROJECT_SPACE", os.getcwd())
 cache_dir = os.path.join(project_space, "my_hf_cache_dir")
@@ -8,6 +9,12 @@ output_path = os.path.join(project_space, "datasets", "FineWeb", "fineweb-10BT.j
 os.makedirs(cache_dir, exist_ok=True)
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-shard = "sample-10BT"
-dataset = load_dataset("HuggingFaceFW/fineweb", shard, cache_dir=cache_dir, split="train")
+ds_name = "HuggingFaceFW/fineweb"
+ds_builder = load_dataset_builder(ds_name)
+print(ds_builder.info.description)
+print(ds_builder.info.features)
+
+
+sample = "sample-10BT"
+dataset = load_dataset(ds_name, name=sample, cache_dir=cache_dir, split="train")
 dataset.to_json(output_path)
